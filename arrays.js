a27b70d958d8ff339574940d0c5b4c7790e94407
Brian Cortes <briancorts166@gmail.com>
2018-06-04 20:02:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ var addElementToBeginningOfArray = function(array, element) {
 
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
 
-  array = [element, ...array]
+  return [element, ...array]
 
-  return array
+array = [element, ...array]
 }
